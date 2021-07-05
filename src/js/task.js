ab7420b36594be61f23c08383d388b26bb7c6256
Katrina <74234305+Katrina-Panina@users.noreply.github.com>
2021-07-05 18:46:25 +0300

    fix

--- src/js/task.js
+++ src/js/task.js
@@ -1,13 +1,13 @@
 export default function superAttack(char) {
   const { special } = char;
-  if (!special) { return null; }
+  if (!special) {
+    return null; 
+  }
   const outputObj = [];
   special.forEach((o) => {
-    const {
-      id, name, description = 'Описание недоступно', icon,
+    const {id, name, description = 'Описание недоступно', icon,
     } = o;
-    outputObj.push({
-      id, name, description, icon,
+    outputObj.push({id, name, description, icon,
     });
   });
   return outputObj;
